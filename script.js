52ce446c613293e5fe3b33bb447309f5d46ec0e2
Julien <Nisouc@outlook.com>
2020-12-13 21:06:27 +0100

    demo for asura

--- script.js
+++ script.js
@@ -9,51 +9,15 @@ but.addEventListener("click", function(){
 
     window.setInterval(disco, 500);
     function disco() {
-        let picker = Math.random() * 10;
-        if (picker <= 1) {
-            discofloor.style.backgroundColor = "red";
-        } else if ( picker <= 2) {
-            discofloor.style.backgroundColor = "orange";
-        } else if ( picker <= 3) {
-            discofloor.style.backgroundColor = "yellow";
-        } else if ( picker <= 4) {
-            discofloor.style.backgroundColor = "green";
-        } else if ( picker <= 5) {
-            discofloor.style.backgroundColor = "blue";
-        } else if ( picker <= 6) {
-            discofloor.style.backgroundColor = "indigo";
-        } else if ( picker <= 7) {
-            discofloor.style.backgroundColor = "purple";
-        } else if ( picker <= 8) {
-            discofloor.style.backgroundColor = "pink";
-        } else if ( picker <= 9) {
-            discofloor.style.backgroundColor = "cyan";
-        } else if ( picker <= 10) {
-            discofloor.style.backgroundColor = "lime";
-        }
 
-        let picker2 = Math.random() * 10;
-        if (picker2 <= 1) {
-            discofloor.style.borderColor = "red";
-        } else if ( picker2 <= 2) {
-            discofloor.style.borderColor = "orange";
-        } else if ( picker2 <= 3) {
-            discofloor.style.borderColor = "yellow";
-        } else if ( picker2 <= 4) {
-            discofloor.style.borderColor = "green";
-        } else if ( picker2 <= 5) {
-            discofloor.style.borderColor = "blue";
-        } else if ( picker2 <= 6) {
-            discofloor.style.borderColor = "indigo";
-        } else if ( picker2 <= 7) {
-            discofloor.style.borderColor = "purple";
-        } else if ( picker2 <= 8) {
-            discofloor.style.borderColor = "pink";
-        } else if ( picker2 <= 9) {
-            discofloor.style.borderColor = "cyan";
-        } else if ( picker2 <= 10) {
-            discofloor.style.borderColor = "lime";
-        }
+        let x = Math.floor(Math.random() * 10);
+        let y = Math.floor(Math.random() * 10);
+
+        let colors = ["red","orange", "yellow", "blue", "indigo", "purple", "pink", "cyan", "lime"];
+
+        discofloor.style.backgroundColor = colors[x];
+        discofloor.style.borderColor = colors[y];
+        
     }
 
     window.setInterval(dancers, 1000);
@@ -64,7 +28,7 @@ but.addEventListener("click", function(){
         dancer.src = "./hyperespe.gif";
         dancer.style.position = "absolute";
         dancer.style.left = Math.random() * 90 + "%";
-        dancer.style.top = Math.random() * 90 + "%";
+        dancer.style.top = Math.random() * 87 + "%";
         dancer.style.transform = "scale(" + Math.random() * 5 +")";
 
         let flip = Math.random();
